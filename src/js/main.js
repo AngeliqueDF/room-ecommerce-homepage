af6b07b404086ea4788c1d36c6f246325d47e644
angelique-df <faye.angelique.d@gmail.com>
2022-08-03 20:37:16 +0000

    Add toggling mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,8 @@ const [sliderLeftButton, sliderRightButton] = Array.from(
 const sliderControls = Slider(".slider-cta ul", ".slide");
 sliderLeftButton.addEventListener("click", sliderControls.moveToPreviousSlide);
 sliderRightButton.addEventListener("click", sliderControls.moveToNextSlide);
+
+const mobileMenuToggle = document.querySelector("header button");
+mobileMenuToggle.addEventListener("click", () => {
+	document.body.classList.toggle("mobile-menu-open");
+});

ef79608c0e6e0ac4cf5affb3dfda7b9120ad702c
angelique-df <faye.angelique.d@gmail.com>
2022-09-19 13:59:55 +0000

    Add slider accessibility

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -3,6 +3,17 @@ export const Slider = function (sliderSelector, slidesSelector) {
 	const slides = document.querySelectorAll(slidesSelector);
 	const viewportWidth = window.innerWidth;
 
+	const announceSlide = (new_current) => {
+		document.querySelector(".slider-cta .liveregion").textContent =
+			"Item " + (new_current + 1) + " of " + slides.length;
+	};
+
+	const liveregion = document.createElement("div");
+	liveregion.setAttribute("aria-live", "polite");
+	liveregion.setAttribute("aria-atomic", "true");
+	liveregion.setAttribute("class", "liveregion visuallyhidden");
+	document.querySelector(".slider-cta").appendChild(liveregion);
+
 	return {
 		moveToPreviousSlide: function () {
 			const itemIndex = Math.floor(slider.scrollLeft / viewportWidth);
@@ -13,13 +24,14 @@ export const Slider = function (sliderSelector, slidesSelector) {
 					left: (slides.length - 1) * viewportWidth,
 					behavior: "smooth",
 				});
+				announceSlide(slides.length - 1);
 				slides[slides.length - 1].focus();
 			} else {
 				slider.scrollTo({
 					left: viewportWidth * previousItemIndex,
 					behavior: "smooth",
 				});
-
+				announceSlide(previousItemIndex);
 				slides[previousItemIndex].focus();
 			}
 		},
@@ -29,12 +41,14 @@ export const Slider = function (sliderSelector, slidesSelector) {
 
 			if (nextItemIndex >= slides.length) {
 				slider.scrollTo({ left: 0, behavior: "smooth" });
+				announceSlide(0);
 				slides[0].focus();
 			} else {
 				slider.scrollTo({
 					left: viewportWidth * nextItemIndex,
 					behavior: "smooth",
 				});
+				announceSlide(nextItemIndex);
 				slides[nextItemIndex].focus();
 			}
 		},
